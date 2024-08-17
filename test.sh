23eca4fafa7149904cf1436179fa65c66cbaa250
pexx <98372449+pex-x@users.noreply.github.com>
2024-08-16 20:32:05 -0600

    Finalized Apache Security, added menu, added removal option.
    The other two don't need much, so I am pretty much done.

--- test.sh
+++ test.sh
@@ -3,40 +3,31 @@ if [[ $EUID -ne 0 ]]; then
   echo "You must be root to run this script."
   exit 1
 elif [[ $EUID == 0 ]]; then
-    echo "Welcome to the LAMP Stack Unified Control Kontroller, or LAMPSUCK. Please choose solution from menu below."
+    echo "Welcome to the LAMP Stack Unified Control Script, or LAMPSUCS. Please choose solution from menu below."
     #Add menu to destroy, and re-run script.
-    read -p "Execute Script (y/n)? "
-    echo 
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        # Make this optional if detected already?
+    echo "1:) Create New Containers"
+    echo "2:) Destroy Containers"
+    echo "3:) Harden Digital Ocean Machine"
+    read -p "Please choose an option. (1/2/3/N)? "
+        if [[ $REPLY =~ 1 ]]; then
         echo "==== Installing Docker ===="
         if [ -x "$(command -v docker)" ]; then
          echo "Docker is installed, updating system & docker to prevent errors."
          sudo apt update
-        else
          sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common
          sudo snap install docker
-         echo
          curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-         echo
          echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-         echo 
          sudo apt update
-         echo
          apt-cache policy docker-ce
          echo
         fi
         read -p "Install Apache? (y/n)? "
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             echo "==== Installing Apache Container ===="
-            docker pull apache2 
             read -p "Please name your container: " NAME_DOCKER
-            echo
             docker run -d --name $NAME_DOCKER -e TZ=UTC -p 8080:80 ubuntu/apache2:2.4-22.04_beta
-            echo ""
-            echo ""
-
-            
+            echo "Apache Security is reccomended..."
             read -p "Do you need Apache Securtiy? (y/n)?" 
             if [[ $REPLY =~ ^[Yy]$ ]]; then
              docker exec -it $NAME_DOCKER /bin/bash
@@ -48,15 +39,31 @@ elif [[ $EUID == 0 ]]; then
 		      echo "Backing up $DOCKER_APACHECONF to $DOCKER_APACHECONF_BK"
 		      cp $DOCKER_APACHECONF $DOCKER_APACHECONF_BK
 		     fi
+
 	         echo "To restore: cp $DOCKER_APACHECONF_BK $DOCKER_APACHECONF"
-        	 echo "Modifying $DOCKER_APACHECONF"
-             sed -i -e '$a\ServerTokens Prods$a\ServerSignature Off/g' $DOCKER_APACHECONF 
-             ehco 'ServerTokens Prod' >> $DOCKER_APACHECONF  
-             echo 'ServerSignature Off' >> $DOCKER_APACHECONF
+        	 echo "Hiding Apache Version and OS Information"
+             sed -i -e '$a\ServerTokens Prods$a\ServerSignature Off/g' $DOCKER_APACHECONF
+             sed -i "KeepAliveTimeout/i 200"
 
+             echo "Disabling Directory Listing"
+             a2dismod --force autoindex
+             echo "Updating Packages/Checking for out of date version"
+             apt update && apt upgrade
 
+             echo "Enabling HTTP Strict Transport Security (HSTS) & restarting server"
+             a2enmod headers
+             service apache2 restart
+             docker start $NAME_DOCKER
+             docker exec -it $NAME_DOCKER /bin/bash
+             config_files=("/etc/apache2/sites-available/default-ssl.conf")
+             insert_string='Header always set Strict-Transport-Security "max-age=31536000; includeSubDomains"\n'
+             sed -i "/<\/VirtualHost>/i  $insert_string" $config_files
 
+             echo "Doing last restart to make sure everything initilized"
+             service apache2 restart
+             docker start $NAME_DOCKER
 
+             echo "Security Complete. To disable unnessecary modules please check apache2ctl -M"
             elif [[ $REPLY =~ ^[Nn]$ ]]; then
              echo
             else
@@ -244,12 +251,52 @@ elif [[ $EUID == 0 ]]; then
             echo "Error. Try again."
             exit 1
         fi
-    elif [[ $REPLY =~ ^[Nn]$ ]]; then
-        echo 
-    else
-        echo "Error. Try again."
-        exit 1
-    fi
+        elif [[ $REPLY =~ 2 ]]; then
+         read -p "Please name your container that you would like to remove: " NAME_REMOVAL
+         docker stop $NAME_REMOVAL
+         docker rm $NAME_REMOVAL
+        elif [[ $REPLY =~ 3 ]]; then
+         echo "Password Requirements"	
+         LOGIN_CONFIG="/etc/login.defs"
+         LOGIN_CONFIG_BACKUP="$LOGIN_CONFIG.backup"
+
+         if ! [[ -f "$LOGIN_CONFIG_BACKUP" ]]; then
+		  echo "backing up $LOGIN_CONFIG to $LOGIN_CONFIG_BACKUP"
+		  cp $LOGIN_CONFIG $LOGIN_CONFIG_BACKUP
+	     fi
+
+    	 echo "To Restore: cp $LOGIN_CONFIG_BACKUP"
+	
+    	 sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS 90/g' $LOGIN_CONFIG
+	     sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS 10/g' $LOGIN_CONFIG
+	     sed -i 's/^PASS_WARN_AGE.*/PASS_WARN_AGE  7/g' $LOGIN_CONFIG
+
+	     echo "Edited $LOGIN_CONFIG"
+	     echo "----Diff----"
+	     git diff --unified=0 --no-index $LOGIN_CONFIG_BACKUP $LOGIN_CONFIG
+         #-----END------#
+
+         #----LIBPAM REQUIREMENTS----#
+         echo "LIBPAM "
+	     sudo apt-get -y install libpam-cracklib
+
+         PW_CONFIG="/etc/pam.d/common-password"
+         PW_CONFIG_BACKUP="$PW_CONFIG.backup"
+
+	     if ! [[ -f "$PW_CONFIG_BACKUP" ]]; then
+		  echo "backing up $PW_CONFIG to $PW_CONFIG_BACKUP"
+		  cp $PW_CONFIG $PW_CONFIG_BACKUP
+	     fi
+         sed -i '1 s/^/password requisite pam_cracklib.so retry=3 minlen=8 difok=3 reject_username minclass=3 maxrepeat=2 dcredit=1 ucredit=1 lcredit=1 ocredit=1\n/' $PW_CONFIG
+
+         echo "Edited $PW_CONFIG"
+	     echo "----Diff----"
+	     git diff --unified=0 --no-index $PW_CONFIG_BACKUP $PW_CONFIG
+        elif [[ $REPLY =~ ^[Nn]$ ]]; then
+         exit
+        else
+            echo "An Error has occurred."
+        fi        
 else
     echo "An Error has occurred."
     exit 1
